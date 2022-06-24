95729d7cd13c8c0046e36391ccd1d61c01a2d5ad
morgan-at-keysight <morgan.allison@keysight.com>
2022-06-24 10:05:42 -0700

    Fixed alignment bit status check logic.

--- Python/xsa_alignment_check.py
+++ Python/xsa_alignment_check.py
@@ -23,8 +23,10 @@ xsa.write('calibration:auto partial')
 alignAllNeeded = int(xsa.query('status:questionable:calibration:condition?')) & (1 << 14)
 alignRFNeeded = int(xsa.query('status:questionable:calibration:condition?')) & (1 << 12)
 
-# If either are 1, run the calibration
-if alignAllNeeded == 1 or alignRFNeeded == 1:
+# alignAllNeeded and alignRFNeeded will either be 1 << 14/1 << 12 or 0
+# If either variable has a non-zero value, it means the respective bit in the status register is high
+# and alignment is needed
+if alignAllNeeded != 0 or alignRFNeeded != 0:
     # The command below is a NON BLOCKING command, which means it doesn't pause operation until it's complete
     # This is actually helpful for us because we can poll a status register to see when it's finished.
     # This is preferable to temporarily setting a long timeout and waiting until the whole thing is done.
